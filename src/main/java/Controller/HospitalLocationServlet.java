3774d2d69080abd43f6ca3a83e01d4a3faf29158
Cornelius <Cornelius@10.17.249.112>
2018-02-21 19:47:33 +0700

    [Edit]-HospitalLocationsServlet.java

--- src/main/java/Controller/HospitalLocationServlet.java
+++ src/main/java/Controller/HospitalLocationServlet.java
@@ -10,6 +10,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -34,13 +36,12 @@ public class HospitalLocationServlet extends HttpServlet {
             throws ServletException, IOException, SQLException {
         response.setContentType("text/html;charset=UTF-8");
         HospitalLocation hosLo = new HospitalLocation();
-        List<HospitalLocation> trashList = HospitalLocation.pullHospitalLocation();
+        List<HospitalLocation> hosLoList = HospitalLocation.pullHospitalLocation();
         request.setAttribute("hosLoList", hosLoList);
         getServletContext().getRequestDispatcher("/Emergency.jsp").forward(request, response);
 
     }
 
-    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -54,7 +55,11 @@ public class HospitalLocationServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+         try {
+             processRequest(request, response);
+         } catch (SQLException ex) {
+             Logger.getLogger(HospitalLocationServlet.class.getName()).log(Level.SEVERE, null, ex);
+         }
     }
 
     /**
@@ -68,7 +73,11 @@ public class HospitalLocationServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+         try {
+             processRequest(request, response);
+         } catch (SQLException ex) {
+             Logger.getLogger(HospitalLocationServlet.class.getName()).log(Level.SEVERE, null, ex);
+         }
     }
 
     /**
@@ -80,5 +89,6 @@ public class HospitalLocationServlet extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
+
 
 }
